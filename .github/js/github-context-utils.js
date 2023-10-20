7dcb74cea29c26e1ac81d8f6c334ce6830337c03
Kent Iversen <kent.iversen@digdir.no>
2023-10-20 11:34:02 +0200

    PF-886 Check if fields is present in JSON payload before extraction (#107)
    * PF-886 Check if fields exists in json payload before extracting them
    
    * PF-886 Use renamed JS method
    
    * PF-886 Update script
    
    * PF-886 Update to checkout v4
    
    * PF-886 Added base64 utils for testing
    
    * Update .github/js/github-context-utils.js
    
    Co-authored-by: Jonas Arneberg Saltvik <jonas.arneberg@gmail.com>
    
    ---------
    
    Co-authored-by: Jonas Arneberg Saltvik <jonas.arneberg@gmail.com>

--- .github/js/github-context-utils.js
+++ .github/js/github-context-utils.js
@@ -1,19 +1,66 @@
-module.exports.getHeadCommitMessage = function(github) {
+module.exports.getCommitMessage = function(github) {
     const obj = JSON.parse(JSON.stringify(github));
-    let message = obj.event.commits[0].message;
-    if (obj.event.hasOwnProperty('head_commit')) {
-        const splitted = obj.event.head_commit.message.split("\n");
-        message = splitted[0];
-    }
-    return message;
-}
+    const commitMessage = (function() {
+        let message = '';
+        if (obj.event.hasOwnProperty('action')) {
+            message = getCommitMessageTriggeredByAction(github);
+        } else if (obj.event.hasOwnProperty('head_commit')) {
+            message = getHeadCommitMessage(github);
+        } else if (obj.event.hasOwnProperty('commits')) {
+            message = getOrdinaryCommitMessage(github);
+        } else {
+            // Do nothing
+        }
 
-module.exports.getRepository = function(github) {
+        return message;
+        }());
+    return commitMessage;
+  }
+
+  module.exports.getRepository = function(github) {
     const json = JSON.parse(JSON.stringify(github));
     return json.repository;
-}
+  }
 
-module.exports.getSHA = function(github) {
+  module.exports.getSHA = function(github) {
     const json = JSON.parse(JSON.stringify(github));
     return json.sha;
-}
+  }
+
+  function getCommitMessageTriggeredByAction(githubContext) {
+    const obj = JSON.parse(JSON.stringify(githubContext));
+    let commitMessage = '';
+    const action = obj.event.action;
+    commitMessage = "No commit message found in Action run: " + action;
+    if (obj.event.hasOwnProperty('client_payload')) {
+        const clientPayload = obj.event.client_payload;
+        if (clientPayload.hasOwnProperty('application-name')) {
+            commitMessage = commitMessage + " for Application(s): " + clientPayload["application-name"];
+        }
+        if (clientPayload.hasOwnProperty('pr-labels')) {
+            commitMessage = commitMessage + " with Labels: " + clientPayload["pr-labels"];
+        }
+        if (clientPayload.hasOwnProperty('pr-number')) {
+            commitMessage = commitMessage + " and PR: #" + clientPayload["pr-number"];
+        }
+    }
+    return commitMessage;
+  }
+
+  function getHeadCommitMessage(githubContext) {
+    const obj = JSON.parse(JSON.stringify(githubContext));
+    let headCommitMessage = '';
+    const message = obj.event.head_commit.message;
+    if (message.includes("\n")) {
+        const splittedMessage = message.split("\n");
+        headCommitMessage = splittedMessage[0];
+    } else {
+        headCommitMessage = message;
+    }
+    return headCommitMessage
+  }
+
+  function getOrdinaryCommitMessage(githubContext) {
+    const obj = JSON.parse(JSON.stringify(githubContext));
+    return obj.event.commits[0].message;
+  }
